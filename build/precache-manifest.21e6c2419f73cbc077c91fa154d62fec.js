7b5c5b8cb119af880b1bb6dc57b2d5adb38691ca
kantaraviteja <kantaraviteja9@gmail.com>
2019-02-13 18:00:31 +0530

    first commit

---
+++ build/precache-manifest.21e6c2419f73cbc077c91fa154d62fec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3415a8e34c45919c2d0",
+    "url": "/static/js/main.e3415a8e.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "e3415a8e34c45919c2d0",
+    "url": "/static/css/main.0d2bb429.chunk.css"
+  },
+  {
+    "revision": "854faf71742cf5a7063716991b246afe",
+    "url": "/index.html"
+  }
+];
